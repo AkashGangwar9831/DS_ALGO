9a44ee2aa1ed7e25ac80b6f2d50dc8f186c75baa
AkashGangwar9831 <akashgangwar0056@gmail.com>
2021-04-19 00:48:49 +0530

    factorial itr

---
+++ Day2/Factorial_itr.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+    int res=1;
+    if(n==0||n==1)
+        return(res);
+    for(int i=2;i<=n;i++)
+    {
+        res=res*i;
+    }
+
+    return(res);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<factorial(n);
+
+    return(0);
+}
