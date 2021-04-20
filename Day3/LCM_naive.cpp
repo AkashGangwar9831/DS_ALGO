45b87811271af2ca850ad6a2b3673511f78830e7
Akash Gangwar <akashgangwar0056@gmail.com>
2021-04-20 16:38:27 +0530

    LCM naive

---
+++ Day3/LCM_naive.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int LCM(int a,int b)
+{
+  int res=max(a,b);
+while(true)
+{
+    if(res%a==0 && res%b==0)
+        return(res);
+    res++;
+}
+  
+  return(res);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<LCM(a,b);
+   
+
+    return(0);
+}
